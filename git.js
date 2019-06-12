513df560bb53c4318f080e6bd803871a854cd402
tanshun123 <129158625git config --global user.email 1291586255@qq.com>
2019-06-12 10:52:33 +0800

    git 练习

---
+++ git.js
@@ -0,0 +1,13 @@
+// alert('你好呀！');
+
+var nowdate = new Date(2017,05,11,08,30,55);
+// console.log(nowdate);
+
+// var time = nowdate.getTime();
+// console.log(time);
+
+
+var month = nowdate.getMonth();
+console.log(month);
+
+
